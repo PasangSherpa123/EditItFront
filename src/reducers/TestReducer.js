4b68995587108f007d68c6199b6da71a7ddd50be
yugal <yugalkhati570@gmail.com>
2021-11-29 22:02:04 +0545

    redux store,root_reducer and actions folder setup

---
+++ src/reducers/TestReducer.js
@@ -0,0 +1,8 @@
+
+export const TestReducer=(state={pasang:"Hero"},action)=>{
+    switch(action.type)
+    {
+        default:
+            return {...state};
+    }
+}
--- src/url.js
+++ src/url.js
@@ -1,5 +1,6 @@
 // http://localhost:5000
 // https://editit101.herokuapp.com
+
 const URL="http://localhost:5000";
 
 module.exports=URL
